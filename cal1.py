6fa8d49899595c247289a1ef0352c42bfa77fcf0
ethanrom <116474403+ethanrom@users.noreply.github.com>
2023-02-08 09:26:04 +0530

    Update cal1.py

--- cal1.py
+++ cal1.py
@@ -21,27 +21,23 @@ def transient_response(step_response, t, steady_state_error):
     return step_response - steady_state_error
 
 k = symbols('k')
-P = 0.25
-Q = 100
-R = 1
+P = float(input("Enter value for P: "))
+Q = float(input("Enter value for Q: "))
+R = float(input("Enter value for R: "))
 t = symbols('t')
 
 M = closed_loop_tf(k, P, Q, R)
-print("Closed loop transfer function:")
+print("\nClosed loop transfer function:")
 print(M)
-print("")
 
 step_response = unit_step_response(M, t)
-print("Unit step response of the closed loop transfer function:")
+print("\nUnit step response of the closed loop transfer function:")
 print(step_response)
-print("")
 
 ss_error = steady_state_error(M, R)
-print("Steady state error:")
+print("\nSteady state error:")
 print(ss_error)
-print("")
 
 transient = transient_response(step_response, t, ss_error)
-print("Transient response:")
+print("\nTransient response:")
 print(transient)
-print("")
